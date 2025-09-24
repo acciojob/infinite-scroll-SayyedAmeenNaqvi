79c6e3537d69d150c32792768106b61b16fbbab9
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-09-24 21:12:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
-//your code here!
+const list = document.getElementById("infi-list");
+let itemCount = 1;
 
+// Function to add items
+function addItems(count) {
+  for (let i = 0; i < count; i++) {
+    const li = document.createElement("li");
+    li.innerText = `Item ${itemCount++}`;
+    list.appendChild(li);
+  }
+}
+
+addItems(10);
+
+
+list.addEventListener("scroll", () => {
+
+  if (list.scrollTop + list.clientHeight >= list.scrollHeight) {
+    addItems(2); 
+  }
+});
